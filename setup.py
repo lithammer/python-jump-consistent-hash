140fde144207594af1dc228007d5b5d5d68cf617
Peter Renström <renstrom.peter@gmail.com>
2015-03-17 11:37:09 +0100

    Add keywords and description.

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@ from setuptools import setup
 
 setup(name='jump_consistent_hash',
       version='1.0.0',
+      description='Implementation of the Jump Consistent Hash algorithm',
       author='Peter Renstroem',
       license='MIT',
       url='https://github.com/renstrom/python-jump-consistent-hash',
       packages=['jump'],
       test_suite='tests',
+      keywords=[
+          'jump hash',
+          'jumphash',
+          'consistent hash',
+          'hash algorithm',
+          'hash'
+      ],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
