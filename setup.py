6d68bfe18ee55f29b9cb2d2e3b3d4de2093e1777
Peter Renström <renstrom.peter@gmail.com>
2015-03-25 22:23:16 +0100

    Bump version (1.0.4)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info < (3, 2):
 
 
 setup(name='jump_consistent_hash',
-      version='1.0.3',
+      version='1.0.4',
       description='Implementation of the Jump Consistent Hash algorithm',
       author='Peter Renström',
       license='MIT',
