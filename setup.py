86056d00556909ec8496e268335229b45edb10dc
Peter Renström <renstrom.peter@gmail.com>
2015-03-30 21:51:32 +0200

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
 # -*- coding: utf-8 -*-
+"""
+Jump Consistent Hash
+--------------------
+
+Python (3) implementation of the jump consistent hash algorithm by John Lamping
+and Eric Veach[1].
+
+Usage
+`````
+
+.. code:: python
+    >>> import jump
+    >>> jump.hash(256, 1024)
+    520
+
+Or if you want to use the C++ extension:
+
+.. code:: python
+    >>> jump.fasthash(256, 1024)
+    520
+
+Links
+`````
+
+[1] http://arxiv.org/pdf/1406.2294v1.pdf
+
+"""
 from __future__ import print_function
 
 import sys
@@ -15,6 +42,7 @@ if sys.version_info < (3, 2):
 setup(name='jump_consistent_hash',
       version='1.1.0',
       description='Implementation of the Jump Consistent Hash algorithm',
+      long_description=__doc__,
       author='Peter Renström',
       license='MIT',
       url='https://github.com/renstrom/python-jump-consistent-hash',
