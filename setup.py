93dc1eaa750e6dec8eedad2353fdabba19c829b1
Peter Renström <renstrom.peter@gmail.com>
2015-05-04 18:34:05 +0200

    Bump version (2.0.1).

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from setuptools import setup, find_packages, Extension
 
 
 setup(name='jump_consistent_hash',
-      version='2.0.0',
+      version='2.0.1',
       description='Implementation of the Jump Consistent Hash algorithm',
       long_description=__doc__,
       author='Peter Renström',
