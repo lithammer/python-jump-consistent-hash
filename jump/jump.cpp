6e9b9be00d9266251a7959e3f1db177891b9a36c
Peter Renström <renstrom.peter@gmail.com>
2015-07-30 10:14:16 +0200

    Run clang-format on the C/C++ code.

--- jump/jump.cpp
+++ jump/jump.cpp
@@ -1,17 +1,15 @@
 extern "C" {
-    #include "jump.h"
+#include "jump.h"
 }
 
-int32_t
-JumpConsistentHash(uint64_t key, int32_t num_buckets)
-{
-    int64_t b = -1, j = 0;
+int32_t JumpConsistentHash(uint64_t key, int32_t num_buckets) {
+  int64_t b = -1, j = 0;
 
-    while (j < num_buckets) {
-        b = j;
-        key = key * 2862933555777941757ULL + 1;
-        j = (b + 1) * (double(1LL << 31) / double((key >> 33) + 1));
-    }
+  while (j < num_buckets) {
+    b = j;
+    key = key * 2862933555777941757ULL + 1;
+    j = (b + 1) * (double(1LL << 31) / double((key >> 33) + 1));
+  }
 
-    return (int32_t)b;
+  return (int32_t)b;
 }
--- jump/jumpmodule.c
+++ jump/jumpmodule.c
@@ -5,49 +5,39 @@
 #define IS_PY3K
 #endif
 
-static PyObject *
-jump_hash(PyObject *self, PyObject *args)
-{
-    uint64_t key;
-    int32_t num_buckets;
+static PyObject *jump_hash(PyObject *self, PyObject *args) {
+  uint64_t key;
+  int32_t num_buckets;
 
-    if (!PyArg_ParseTuple(args, "Li", &key, &num_buckets)) {
-        return NULL;
-    }
+  if (!PyArg_ParseTuple(args, "Li", &key, &num_buckets)) {
+    return NULL;
+  }
 
-    if (num_buckets < 1) {
-        PyErr_SetString(PyExc_ValueError, "num_buckets must be a positive number");
-        return NULL;
-    }
+  if (num_buckets < 1) {
+    PyErr_SetString(PyExc_ValueError, "num_buckets must be a positive number");
+    return NULL;
+  }
 
-    int32_t h = JumpConsistentHash(key, num_buckets);
-    return Py_BuildValue("i", h);
+  int32_t h = JumpConsistentHash(key, num_buckets);
+  return Py_BuildValue("i", h);
 }
 
 static PyMethodDef JumpMethods[] = {
     {"hash", jump_hash, METH_VARARGS, "Jump Consistent Hash"},
-    {NULL, NULL, 0, NULL}
-};
+    {NULL, NULL, 0, NULL}};
 
 #if PY_MAJOR_VERSION >= 3
-static struct PyModuleDef jumpmodule = {
-    PyModuleDef_HEAD_INIT,
-    "jump",
-    NULL,
-    -1,
-    JumpMethods
-};
+static struct PyModuleDef jumpmodule = {PyModuleDef_HEAD_INIT, "jump", NULL, -1,
+                                        JumpMethods};
 
-PyMODINIT_FUNC
-PyInit__jump(void)
+PyMODINIT_FUNC PyInit__jump(void)
 #else
-void
-init_jump(void)
+void init_jump(void)
 #endif
 {
 #if PY_MAJOR_VERSION >= 3
-    return PyModule_Create(&jumpmodule);
+  return PyModule_Create(&jumpmodule);
 #else
-    (void) Py_InitModule("_jump", JumpMethods);
+  (void)Py_InitModule("_jump", JumpMethods);
 #endif
 }
