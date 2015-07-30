98cdd545b344f636ea3d1758f049dd851ea9d921
Peter Renström <renstrom.peter@gmail.com>
2015-07-30 10:55:58 +0200

    Add docstring to fasthash.

--- jump/jumpmodule.c
+++ jump/jumpmodule.c
@@ -5,6 +5,18 @@
 #define IS_PY3K
 #endif
 
+PyDoc_STRVAR(doc_hash, "Generate a number in the range [0, num_buckets).\n\n\
+This function uses C bindings for speed.\n\n\
+Args:\n\
+    key (int): The key to hash.\n\
+    num_buckets (int): Number of buckets to use.\n\n\
+Returns:\n\
+    The bucket number `key` computes to.\n\n\
+Raises:\n\
+    ValueError: If `num_buckets` is not a positive number.\n");
+
+PyDoc_STRVAR(doc_jump, "Fast, minimal memory, consistent hash algorithm.");
+
 static PyObject *jump_hash(PyObject *self, PyObject *args) {
   uint64_t key;
   int32_t num_buckets;
@@ -22,9 +34,8 @@ static PyObject *jump_hash(PyObject *self, PyObject *args) {
   return Py_BuildValue("i", h);
 }
 
-static PyMethodDef JumpMethods[] = {
-    {"hash", jump_hash, METH_VARARGS, "Jump Consistent Hash"},
-    {NULL, NULL, 0, NULL}};
+static PyMethodDef JumpMethods[] = {{"hash", jump_hash, METH_VARARGS, doc_hash},
+                                    {NULL, NULL, 0, NULL}};
 
 #if PY_MAJOR_VERSION >= 3
 static struct PyModuleDef jumpmodule = {PyModuleDef_HEAD_INIT, "jump", NULL, -1,
