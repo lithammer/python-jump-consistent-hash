433c74417238fa6224c7994c4b3cb5cb3f6464e1
Peter Renström <renstrom.peter@gmail.com>
2015-09-03 22:40:31 +0200

    Jumped the gun on Python 3.5

--- setup.py
+++ setup.py
@@ -81,6 +81,5 @@ setup(name='jump_consistent_hash',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.2',
           'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5'
+          'Programming Language :: Python :: 3.4'
       ])
